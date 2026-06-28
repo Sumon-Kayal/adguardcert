fe0ac187a9086a3688bf77a95b98168d4c2b9535
Sumon Kayal <57747396+Sumon-Kayal@users.noreply.github.com>
2026-06-29 01:11:42 +0530

    Add files via upload

--- dist.sh
+++ dist.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -e
 
 UPDATE_BINARY_URL="https://raw.githubusercontent.com/topjohnwu/Magisk/master/scripts/module_installer.sh"
 
 mkdir -p ./module/META-INF/com/google/android
-curl "${UPDATE_BINARY_URL}" > ./module/META-INF/com/google/android/update-binary
+curl -fsSL "${UPDATE_BINARY_URL}" -o ./module/META-INF/com/google/android/update-binary
 echo "#MAGISK" > ./module/META-INF/com/google/android/updater-script
 
 VERSION=$(sed -ne "s/version=\(.*\)/\1/gp" ./module/module.prop)
 NAME=$(sed -ne "s/id=\(.*\)/\1/gp" ./module/module.prop)
 
-rm -f ${NAME}-${VERSION}.zip
+rm -f "${NAME}-${VERSION}.zip"
 (
   cd ./module
-  zip ../${NAME}-${VERSION}.zip -r * -x ".*" "*/.*"
+  zip "../${NAME}-${VERSION}.zip" -r * -x ".*" "*/.*"
 )
